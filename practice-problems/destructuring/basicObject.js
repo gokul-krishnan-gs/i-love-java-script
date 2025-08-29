55c558dfaa73d584bc2c78e4c6a4802ce6e6edae
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-08-29 20:25:11 +0530

    Create basicObject.js

---
+++ practice-problems/destructuring/basicObject.js
@@ -0,0 +1,7 @@
+const person = { name: "Gokul", age: 20, city: "Chennai" };
+// Extract name and age using destructuring
+const {name,age} = person;
+
+console.log(person);
+console.log(name);
+console.log(age);
