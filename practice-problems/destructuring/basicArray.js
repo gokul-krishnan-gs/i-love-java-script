360375be0bef82f8ca041f1245bfced659218c7a
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-08-29 20:12:25 +0530

    Create basicArray.js

---
+++ practice-problems/destructuring/basicArray.js
@@ -0,0 +1,7 @@
+const numbers = [10, 20, 30, 40];
+// Extract first and second elements using destructuring
+const [num1,num2]= numbers;
+
+console.log(numbers);
+console.log(num1);
+console.log(num2);
