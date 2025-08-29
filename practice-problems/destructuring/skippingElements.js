d06d401afc55d6cf5aca5f09d38cc805988901dd
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-08-29 20:13:50 +0530

    Create skippingElements.js

---
+++ practice-problems/destructuring/skippingElements.js
@@ -0,0 +1,8 @@
+const colors = ["red", "green", "blue", "yellow"];
+// Use destructuring to get "red" and "blue"
+
+const [red, , blue] = colors;
+
+console.log(colors);
+console.log(red);
+console.log(blue);
