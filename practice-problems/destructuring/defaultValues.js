4f221f38ec3c83f6d44d4f091d17210a28736b6f
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-08-29 20:15:52 +0530

    Create defaultValues.js

---
+++ practice-problems/destructuring/defaultValues.js
@@ -0,0 +1,8 @@
+const fruits = ["apple"];
+// Assign first element to fruit1 and second element to fruit2 with default "banana"
+
+const [fruit1,fruit2 = "banana"] = fruits;
+
+console.log(fruits);
+console.log(fruit1);
+console.log(fruit2);
