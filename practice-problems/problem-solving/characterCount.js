61f3af30f9dc5feda1889b287439a9e6f0c17bfb
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-12-03 07:52:03 +0530

    Count how many times a character appears in a string

---
+++ practice-problems/problem-solving/characterCount.js
@@ -0,0 +1,12 @@
+function characterCount(word,letter){
+    let count = 0;
+    for(let i=0;i<word.length;i++){
+        if(word[i]===letter)
+            count ++;
+    }
+    return count;
+}
+
+const result = characterCount("aabcad","a");
+
+console.log(result);
