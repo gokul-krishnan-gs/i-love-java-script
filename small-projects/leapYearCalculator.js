1081ae02f65c579b75b656188314da254099c998
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-12-10 11:13:14 +0530

    Add leap year calculator function

---
+++ small-projects/leapYearCalculator.js
@@ -0,0 +1,19 @@
+function isLeapYear(year){
+  if (year % 400 === 0) {
+    return `${year} is a leap year.`;
+  }
+  else if (year % 100 === 0) {
+    return `${year} is not a leap year.`;
+  }
+  else if (year % 4 === 0) {
+    return `${year} is a leap year.`;
+  }
+  else {
+    return `${year} is not a leap year.`;
+  }
+}
+
+const year = 2024;
+const result = isLeapYear(year);
+
+console.log(result);
