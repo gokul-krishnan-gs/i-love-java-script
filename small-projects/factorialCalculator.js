432e66ae713390453c03cffea576deb32148c0bb
Gokul Krishnan <gokulkrishnangs02@gmail.com>
2025-12-13 18:19:29 +0530

    Add factorial calculator implementation

---
+++ small-projects/factorialCalculator.js
@@ -0,0 +1,14 @@
+let num = 5;
+function factorialCalculator(number){
+ let result = 1;
+  for(let i=1;i<=number;i++){
+    result = i * result;
+  }
+return result;
+}
+
+const factorial = factorialCalculator(num);
+const resultMsg = `Factorial of ${num} is ${factorial}`;
+
+console.log(resultMsg)
+
